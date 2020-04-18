027af7155ab786f91229fcfe755bc18238d7e28f
UltraNote <support@ultranote.org>
2020-04-18 09:27:18 +0200

    branding

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-        'api' => 'https://explorer.conceal.network/daemon',
-        'pool' => 'https://explorer.conceal.network/pool',
+        'api' => 'http://node.ultranote.org:31000'',
+        'pool' => 'https://explorer.ultranote.org/pool',
         'blockTargetInterval' => 120,
         'coinUnits' => 1000000,
         'maple' => 'https://maplechange.com/api/v2/tickers/ccxbtc.json',
