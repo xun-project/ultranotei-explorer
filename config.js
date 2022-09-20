db8fb19b8dcd43c1ac2b6e2a54ebd8650969fbe1
UltraNote <support@ultranote.org>
2022-09-20 19:28:52 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+var api = 'https://explorer.ultranote.org/daemon';
+var poolListUrl = 'https://explorer.ultranote.org/services/pools/data';
+var donationAddress = "";
+var blockTargetInterval = 120;
+var coinUnits = 1000000;
+var symbol = 'XUNI';
+var refreshDelay = 5000;
